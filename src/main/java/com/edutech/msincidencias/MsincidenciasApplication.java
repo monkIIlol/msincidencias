b53b70e51b99bb85c6e70b8eded9588faf8ba01e
Cristian Urcullu <cr.urcullu@duocuc.cl>
2025-05-14 13:03:38 -0400

    first commit

---
+++ src/main/java/com/edutech/msincidencias/MsincidenciasApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.msincidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsincidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsincidenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edutech/msincidencias/MsincidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.msincidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsincidenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
