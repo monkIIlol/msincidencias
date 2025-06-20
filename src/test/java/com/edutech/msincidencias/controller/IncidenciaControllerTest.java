b03e601b9e5930480f6e2de3239471f04508afe0
Cristian Urcullu <cr.urcullu@duocuc.cl>
2025-06-20 18:37:06 -0400

    primer controllerTest

--- src/test/java/com/edutech/msincidencias/controller/IncidenciaControllerTest.java
+++ src/test/java/com/edutech/msincidencias/controller/IncidenciaControllerTest.java
@@ -1,14 +1,77 @@
 package com.edutech.msincidencias.controller;
 
+import static org.mockito.ArgumentMatchers.isA;
+
+import java.time.LocalDate;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.edutech.msincidencias.model.Incidencia;
 import com.edutech.msincidencias.repository.IncidenciaRepository;
 import com.edutech.msincidencias.service.IncidenciaService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class IncidenciaControllerTest {
+@WebMvcTest(IncidenciaController.class)
+class IncidenciaControllerTest {
+    LocalDate fechaIncidencia = LocalDate.now();
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IncidenciaService incidenciaService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Incidencia incidencia;
+
+    @BeforeEach
+    void setUp() {
+        incidencia = new Incidencia(1L, 10, 5, "titulo", "descripcion", LocalDate.now(), "ABIERTO", "ALTA", true);
+    }
+
+    @Test
+    public void testGetAllIncidencias() throws Exception {
+        when(incidenciaService.findAll()).thenReturn(List.of(incidencia));
+
+        mockMvc.perform(get("api/v1/incidencias"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("[0].idIncidencia").value(1L));
+    }
+
+    @Test
+    public void testReadIncidencia() throws Exception {
+
+    }
+
+    @Test
+    public void testEstado() throws Exception {
+
+    }
+
+    @Test
+    public void testPostIncidencia() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateIncidencia() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteIncidencia() throws Exception {
+
+    }
 }
